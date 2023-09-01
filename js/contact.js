a1a4ef577bdcb4dfe1da2bc50d3a3df0b2254db6
Amalia Minzat <minzatamalia010@gmail.com>
2023-09-01 15:15:28 +0300

    Changes made to responsivness and contact form.

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,20 @@
-function validateForm() {
-    let x = document.forms["myForm"]["First Name"]["Last name"]["Enter your email adress"]["Enter your subject"]["Message"].value;
-    if (x == "") {
-      alert("This field is manadatory to be filled");
+  function validateForm(event) {
+    // Prevent the default form submission
+    event.preventDefault();
+  
+    // Retrieve values of the form fields
+    let firstName = document.forms["myForm"]["fname"].value;
+    let lastName = document.forms["myForm"]["lname"].value;
+    let email = document.forms["myForm"]["mail"].value;
+    let subject = document.forms["myForm"]["subject"].value;
+    let message = document.forms["myForm"]["message"].value;
+  
+    // Check if any of the fields is empty
+    if (firstName === "" || lastName === "" || email === "" || subject === "" || message === "") {
+      alert("All fields are mandatory and must be filled.");
       return false;
     }
+  
+    return true; // If all fields are filled, the form can be submitted
   }
+  
