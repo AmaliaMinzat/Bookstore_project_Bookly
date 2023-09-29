3f047df7f7eae3a3aa23d9b0de04768e95a25f92
Amalia Minzat <minzatamalia010@gmail.com>
2023-09-29 12:09:11 +0300

    Validation form update

--- js/contact.js
+++ js/contact.js
@@ -8,13 +8,49 @@
     let email = document.forms["myForm"]["mail"].value;
     let subject = document.forms["myForm"]["subject"].value;
     let message = document.forms["myForm"]["message"].value;
-  
+    let error = 0;
+
     // Check if any of the fields is empty
-    if (firstName === "" || lastName === "" || email === "" || subject === "" || message === "") {
-      alert("All fields are mandatory and must be filled.");
-      return false;
+    if(firstName===""){
+      document.getElementById("fname-error").style.display = "block";
+      error=1;
+    }
+    else{
+      document.getElementById("fname-error").style.display = "none";
+    }
+    if(firstName===""){
+      document.getElementById("lname-error").style.display = "block";
+      error=1;
+    }
+    else{
+      document.getElementById("lname-error").style.display = "none";
+    }
+    if(email===""){
+      document.getElementById("email-error").style.display = "block";
+      error=1;
+    }
+    else{
+      document.getElementById("email-error").style.display = "none";
+    }
+    if(subject===""){
+      document.getElementById("subj-error").style.display = "block";
+      error=1;
+    }
+    else{
+      document.getElementById("subj-error").style.display = "none";
+    }
+    if(message===""){
+      document.getElementById("msg-error").style.display = "block";
+      error=1;
+    }
+    else{
+      document.getElementById("msg-error").style.display = "none";
+    }
+    
+    if(error==0){
+      alert("Message sent");
     }
   
-    return true; // If all fields are filled, the form can be submitted
+    //return true; // If all fields are filled, the form can be submitted
   }
   
